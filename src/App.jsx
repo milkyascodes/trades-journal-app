3243d0133003dbdeb0990dd931d65cceb7eebf2c
miki <milkyas.dev@gmail.com>
2026-03-18 18:11:25 -0300

    initalised components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1>Welcome</h1>;
+  return <></>;
 }
 
 export default App;
--- src/features/noteSlice.jsx
+++ src/features/noteSlice.jsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
   notes: [],
@@ -11,11 +11,24 @@ const noteSlice = createSlice({
   name: "notes",
   initialState,
   reducers: {
-    addNote() {},
+    addNote(state, action) {
+      const note = action.payload;
+      state.notes.push({
+        ...state,
+        id: nanoid(),
+        title: note.title,
+        reasonForTrade: note.reason,
+        lessonLearned: note.lesson,
+        stopLoss: note.stopLoss,
+        takeProfit: note.takeProfit,
+        image: "",
+        status: note.status,
+      });
+    },
     deleteNote() {},
     updateNote() {},
   },
 });
 
 export const { addNote, deleteNote, updateNote } = noteSlice.actions;
-export const noteReducer = noteSlice.reducer;
+export default noteSlice.reducer;
